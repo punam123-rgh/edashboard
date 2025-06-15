9f3e857fab77873f8efbf5ac9465dbdbc3c6e826
punam123--rgh <gondhalepunam123@gmail.com>
2025-06-15 20:55:50 +0530

    modify

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require ('express');
 const cors = require('cors')
 const app = express();
+require('dotenv').config();
+
 const productRoutes=require('./router/productRouter');
 const userRouter = require('./router/userRouter')
 const connectDb=require('./config/db')
 const port =process.env.PORT
+const front =process.env.FRONTEND_URL
+
 app.use(express.json());
-app.use(cors())
+
+const corsOptions ={
+  origin:front,
+  methods:["GET","POST","PUT","DELETE"],
+  allowedHeaders :["Content-Type","authorization"]
+}
+app.use(cors(corsOptions))
 connectDb();
 
 app.use('/api',productRoutes)
