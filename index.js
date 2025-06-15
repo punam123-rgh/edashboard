6ccbf00cdd6eab2a92685012c8dee2efa4631a2c
punam123--rgh <gondhalepunam123@gmail.com>
2025-06-15 20:27:36 +0530

    modify gitignore file

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require ('express');
+const cors = require('cors')
 const app = express();
 const productRoutes=require('./router/productRouter');
 const userRouter = require('./router/userRouter')
 const connectDb=require('./config/db')
 const port =process.env.PORT
 app.use(express.json());
+app.use(cors())
 connectDb();
-app.get('/ ',(req,resp) => {
-resp.send('hello')
-})
+
 app.use('/api',productRoutes)
 app.use('/apiuser',userRouter)
 app.listen(port,()=>{
