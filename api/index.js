301e6d087c873838990a83703a2dd83634bb2a9e
punam123--rgh <gondhalepunam123@gmail.com>
2025-06-16 17:48:24 +0530

    modify

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
 const express = require ('express');
 const cors = require('cors')
+const serverless = require('serverless-http');
 const app = express();
 require('dotenv').config();
 
-const productRoutes=require('./router/productRouter');
-const userRouter = require('./router/userRouter')
-const connectDb=require('./config/db')
+const productRoutes=require('../router/productRouter');
+const userRouter = require('../router/userRouter')
+const connectDb=require('../config/db')
 const port =process.env.PORT
 const front =process.env.FRONTEND_URL
 
@@ -24,3 +25,5 @@ app.use('/apiuser',userRouter)
 app.listen(port,()=>{
   console.log(`app listning on port ${port}`)
 });
+module.exports = app;
+module.exports.handler = serverless(app)
--- db/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-    name:String,
-    email:String,
-    password:String
-
-});
-module.exports= mongoose.model("users",userSchema);
-//In Mongoose, a model is a wrapper for a Mongoose schema that provides an interface to interact with a specific MongoDB collection.
